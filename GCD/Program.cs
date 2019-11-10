ceab98d5837b54415c58d57b8590dcde45cb8332
Sumit Mishra <sumitmishra2020@outlook.com>
2019-11-10 14:06:00 +0530

    GCD

---
+++ GCD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GCD
+{
+    class Program
+    {
+        static int GCD(int a, int b)
+        {
+            while(b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The GCD of 56, 24 is: " + GCD(56, 24));
+        }
+    }
+}
