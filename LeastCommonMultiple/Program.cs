9dce96b1c75759c0ee793148f72c97aa8ac38828
Sumit Mishra <sumitmishra2020@outlook.com>
2019-11-10 14:29:22 +0530

    Least common multiple

---
+++ LeastCommonMultiple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeastCommonMultiple
+{
+    class Program
+    {
+        static int GCD(int a, int b)
+        {
+            for(int t = b;b!=0;t = b)
+            {
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        static int LeastCommonMultiple(int a, int b)
+        {
+            return (a * b) / GCD(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Least Common Multiple of 6, 8 is: " + LeastCommonMultiple(6, 8));
+        }
+    }
+}
